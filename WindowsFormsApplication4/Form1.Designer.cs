574b5b9e4362e0b9945f45c0f700cfcee09ce6a6
Christian Neubauer <cneubauern@googlemail.com>
2015-04-07 10:04:09 +0200

    initial commit

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 101);
+            this.textBox1.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(197, 97);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "list1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "list2";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(74, 38);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "loops";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(74, 67);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(295, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(423, 237);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(74, 97);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 9;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "rngInv";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int[] list_1 = new int[] {2,10,8,3,5,4};
+        int[] list_2 = new int[] {8,10,7,2,6};
+        List<int> numbers = new List<int>();
+
+        int count = 0; 
+        
+          
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private int findGCI(int[] list1, int[] list2)
+        {
+            int gci = 0;
+
+            for (int i = 0; i < list1.Count(); i++)
+            {
+                for (int j = 0; j < list2.Count(); j++)
+                {
+                    if (list1[i] == list2[j]){
+                       if (list1[i] > gci)
+                        gci = list1[i];
+                    }
+                }
+            }
+            return gci;
+        }
+
+        private void generateRndLists() 
+        {
+            list_1 = generateRndList(Convert.ToInt32(numericUpDown1.Value));
+            list_2 = generateRndList(Convert.ToInt32(numericUpDown2.Value));
+        }
+
+        private int[] generateRndList(int rnd)
+        {
+            int[] rndList = new int[rnd];
+
+            generateNumbers();
+            Random rng = new Random();
+
+            for (int i = 0; i < rnd; i++)
+            {
+                int rdx = rng.Next(numbers.Count);
+                rndList[i] = numbers[rdx];
+                numbers.RemoveAt(rdx);
+            }
+
+                return rndList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+            Stopwatch timer = new Stopwatch();
+            List<long> listCPUTimes = new List<long>();
+            int loops = Convert.ToInt32(numericUpDown3.Value);
+            int n = 0;
+
+            while (n != loops)
+            {
+                generateRndLists();
+                timer.Start();
+                int gci = findGCI(list_1, list_2);
+                timer.Stop();
+                listCPUTimes.Add(timer.ElapsedTicks);
+
+                textBox1.AppendText("\r\nList 1: " + string.Join(",", list_1));
+                textBox1.AppendText("\r\nList 2: " + string.Join(",", list_2));
+
+
+                textBox1.AppendText("\r\nGreatest Common Integer is " + gci);
+                textBox1.AppendText("\r\nDuration " + timer.ElapsedTicks + " ticks");
+                
+                timer.Reset();
+
+                n++;
+            }
+
+            double mean = getMean(listCPUTimes);
+            double SD = getSD(listCPUTimes);
+
+            textBox1.AppendText("\r\nMean is " + mean);
+            textBox1.AppendText("\r\nStandard Deviation is " + SD);
+
+            chart1.ChartAreas[0].AxisY.Maximum = 100;
+
+            chart1.Series.Add("Mean" + count);
+            chart1.Series["Mean" + count].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            chart1.Series.Add("SD" + count);
+            chart1.Series["SD" + count].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.ErrorBar;
+
+            chart1.Series["Mean" + count].Points.AddXY(count, mean);
+            chart1.Series["SD" + count].Points.AddXY(count, mean, mean - SD / 2, mean + SD / 2);
+            count++;
+
+        }
+
+        private void generateNumbers()
+        {
+            for (int i = 0; i < Convert.ToInt32(numericUpDown4.Value); i++)
+                numbers.Add(i);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static double getMean(List<long> resultset)
+        {
+            //ToDo: your implementation
+            ulong number = Convert.ToUInt64(resultset.Count);
+            ulong x = 0;
+            foreach (ulong time in resultset)
+            {
+                x += time;
+            }
+
+            return x / number;
+        }
+
+        public static double getSD(List<long> resultSet)
+        {
+            //ToDo: your implementation
+            int v = 0;
+            int n = resultSet.Count();
+            int m = (int)getMean(resultSet);
+            foreach (int x in resultSet)
+            {
+                v += (x - m) * (x - m);
+            }
+            return Math.Sqrt(v / n);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+
+}
